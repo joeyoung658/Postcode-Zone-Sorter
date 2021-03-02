81d7f15ec4fb7b7efda00c93b8d41b1c3de9c96a
Joseph Young <josephyoung658@gmail.com>
2021-03-02 15:12:35 +0000

    Finished

---
+++ sorter.py
@@ -0,0 +1,45 @@
+monday = ["SE1","SE16","EC1W","E1","E2","N1","N1C","NW1","N19","W2","FW1","W1","WC1","WC2","EC1","EC2","EC3","EC4","SW1A","SW1E","SW1H","SW1P","SW1V","SW1W","SW1X","SW1Y","W1B","W1C","W1D","W1F","W1G","W1H","W1J","W1K","W1S","W1T","W1U","W1W","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2R","EC1A","EC1M","EC1N","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3R","EC3V","EC4A","EC4M","EC4N","EC4R","EC4V","EC4Y","NW5","SE11","SE17","N4","N15","N5","N7","CM15","CM14","RM3","RM2","RM1","RM6","CM13","CM12","CM4","RM7","IG2","IG4","E11","E17","CM11","TN15","TN14"]
+tuesday = ["CM1","CM2","CM3","CM6","SE1","EC1W","E1","E2","N1C","NW1","W2","FW1","W1","WC1","WC2","EC1","EC2","EC3","EC4","SW1A","SW1E","SW1H","SW1P","SW1V","SW1W","SW1X","SW1Y","W1B","W1C","W1D","W1F","W1G","W1H","W1J","W1K","W1S","W1T","W1U","W1W","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2R","EC1A","EC1M","EC1N","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3R","EC3V","EC4A","EC4M","EC4N","EC4R","EC4V","EC4Y","SE11","N5","N7","CM15","CM14","RM3","RM2","RM1","RM6","CM13","CM12","CM4","RM7","IG2","IG4","E11","E10","E9","SW13","SW6","SW10","SW3","SW11","SW8","RM18","RM16","RM14","CM11"]
+wednesday = ["SE1","SE16","EC1W","E1","E2","N1","N1C","NW1","N19","W2","FW1","W1","WC1","WC2","EC1","EC2","EC3","EC4","SW1A","SW1E","SW1H","SW1P","SW1V","SW1W","SW1X","SW1Y","W1B","W1C","W1D","W1F","W1G","W1H","W1J","W1K","W1S","W1T","W1U","W1W","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2R","EC1A","EC1M","EC1N","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3R","EC3V","EC4A","EC4M","EC4N","EC4R","EC4V","EC4Y","NW5","SE11","SE17","N4","N15","N5","N7","CM15","CM14","RM3","RM2","RM1","RM6","CM13","CM12","CM4","RM7","IG2","IG4","E11","E17"]
+thursday = ["CM1","CM2","CM3","CM6","SE1","EC1W","E1","E2","N1C","NW1","W2","FW1","W1","WC1","WC2","EC1","EC2","EC3","EC4","SW1A","SW1E","SW1H","SW1P","SW1V","SW1W","SW1X","SW1Y","W1B","W1C","W1D","W1F","W1G","W1H","W1J","W1K","W1S","W1T","W1U","W1W","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2R","EC1A","EC1M","EC1N","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3R","EC3V","EC4A","EC4M","EC4N","EC4R","EC4V","EC4Y","SE11","N5","N7","CM15","CM14","RM3","RM2","RM1","RM6","CM13","CM12","CM4","RM7","IG2","IG4","E11","E10","E9","SW13","SW6","SW10","SW3","SW11","SW8","RM18","RM16","RM14","CM11"]
+friday = ["CM1","CM2","CM3","CM6","SE1","EC1W","E1","E2","N1C","NW1","W2","FW1","W1","WC1","WC2","EC1","EC2","EC3","EC4","SW1A","SW1E","SW1H","SW1P","SW1V","SW1W","SW1X","SW1Y","W1B","W1C","W1D","W1F","W1G","W1H","W1J","W1K","W1S","W1T","W1U","W1W","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2R","EC1A","EC1M","EC1N","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3R","EC3V","EC4A","EC4M","EC4N","EC4R","EC4V","EC4Y","SE11","N5","N7","CM15","CM14","RM3","RM2","RM1","RM6","CM13","CM12","CM4","RM7","IG2","IG4","E11","E10","E9","SW13","SW6","SW10","SW3","SW11","SW8","RM18","RM16","RM14","CM11","SE16","N1","N19","NW5","SE17","N4","N15","E17","TN15","TN14"]
+saturday = ["CM2","CM4","CM15","CM11","CM12","CM13","CM14","CM15","EC3M","EC3N","EC3R","NW2","NW3","NW4","NW6","NW7","NW8","NW9","NW10","NW11","IG2","IG4","RM3","RM6","RM7","SS12","SS13","SS14","SS15","SE1"]
+
+"""def postCodeSorter(day, zones, array):
+    for postcode in array:
+        zonesArrayLen = len(zones)
+        if (zonesArrayLen == 0):
+            zones.append([[postcode],[day]])
+
+        for i in range(zonesArrayLen):
+            if (zones[i][0] == postcode):
+                zones[i][1].append(day)
+            else:
+                zones.append([[postcode],[day]])
+
+    return zones"""
+
+def postCodeSorter(day, zones, array):
+    for postcode in array:
+        found = False
+        for i in range(len(zones)):
+            if (zones[i][0] == postcode):
+                zones[i][1].append(day)
+                found = True
+        if (found == False):
+            zones.append([postcode, [day]])
+    return zones        
+
+
+
+zones = []
+
+zones = postCodeSorter("monday", zones, monday)
+zones = postCodeSorter("tuesday", zones, tuesday)
+zones = postCodeSorter("wednesday", zones, wednesday)
+zones = postCodeSorter("thursday", zones, thursday)
+zones = postCodeSorter("friday", zones, friday)
+zones = postCodeSorter("saturday", zones, saturday)
+
+
+print(zones)
