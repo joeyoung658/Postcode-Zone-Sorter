add215e62f26c13bce63935224becd0c9827018f
Joseph Young <josephyoung658@gmail.com>
2021-03-05 15:54:44 +0000

    Updates

--- sorter.py
+++ sorter.py
@@ -1,3 +1,8 @@
+import csv
+
+
+
+
 monday = ["SE1","SE16","EC1W","E1","E2","N1","N1C","NW1","N19","W2","FW1","W1","WC1","WC2","EC1","EC2","EC3","EC4","SW1A","SW1E","SW1H","SW1P","SW1V","SW1W","SW1X","SW1Y","W1B","W1C","W1D","W1F","W1G","W1H","W1J","W1K","W1S","W1T","W1U","W1W","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2R","EC1A","EC1M","EC1N","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3R","EC3V","EC4A","EC4M","EC4N","EC4R","EC4V","EC4Y","NW5","SE11","SE17","N4","N15","N5","N7","CM15","CM14","RM3","RM2","RM1","RM6","CM13","CM12","CM4","RM7","IG2","IG4","E11","E17","CM11","TN15","TN14"]
 tuesday = ["CM1","CM2","CM3","CM6","SE1","EC1W","E1","E2","N1C","NW1","W2","FW1","W1","WC1","WC2","EC1","EC2","EC3","EC4","SW1A","SW1E","SW1H","SW1P","SW1V","SW1W","SW1X","SW1Y","W1B","W1C","W1D","W1F","W1G","W1H","W1J","W1K","W1S","W1T","W1U","W1W","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2R","EC1A","EC1M","EC1N","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3R","EC3V","EC4A","EC4M","EC4N","EC4R","EC4V","EC4Y","SE11","N5","N7","CM15","CM14","RM3","RM2","RM1","RM6","CM13","CM12","CM4","RM7","IG2","IG4","E11","E10","E9","SW13","SW6","SW10","SW3","SW11","SW8","RM18","RM16","RM14","CM11"]
 wednesday = ["SE1","SE16","EC1W","E1","E2","N1","N1C","NW1","N19","W2","FW1","W1","WC1","WC2","EC1","EC2","EC3","EC4","SW1A","SW1E","SW1H","SW1P","SW1V","SW1W","SW1X","SW1Y","W1B","W1C","W1D","W1F","W1G","W1H","W1J","W1K","W1S","W1T","W1U","W1W","WC1A","WC1B","WC1E","WC1H","WC1N","WC1R","WC1V","WC1X","WC2A","WC2B","WC2E","WC2H","WC2R","EC1A","EC1M","EC1N","EC1R","EC1V","EC1Y","EC2A","EC2M","EC2N","EC2R","EC2V","EC2Y","EC3A","EC3M","EC3N","EC3R","EC3V","EC4A","EC4M","EC4N","EC4R","EC4V","EC4Y","NW5","SE11","SE17","N4","N15","N5","N7","CM15","CM14","RM3","RM2","RM1","RM6","CM13","CM12","CM4","RM7","IG2","IG4","E11","E17"]
@@ -37,10 +42,14 @@ def zoneCreater(zones):
             counter += 1
             newZones.append([counter, [zones[i][0]], zones[i][1], zones[i][2]])
     return newZones
-        
-def suttonsilver_bobtailmanagment_zones():
-    #Days that require a delivery should not be included in the list
+
+def zone_delivery_days():
     zone_delivery_days = [0,1,2,3,4,5,6]
+    return    
+        
+def suttonsilver_bobtailmanagment_zones(zones):
+    #Days that require a delivery should not be included in the list so keys need to be swaped.
+    
     tableHeaders = ["zones_id", "zone_name", "price_default", "zone_times", "status", "zone_delivery_days"]
     
     return
